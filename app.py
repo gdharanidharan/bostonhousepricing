1da6b79ab342c4feac342f337ce5a264e5b26aa9
gdharani1996 <gdharani1996@gmail.com>
2022-10-10 21:54:14 +0530

    Uploaded

--- app.py
+++ app.py
@@ -15,16 +15,25 @@ def home():
     return render_template('home.html')
 
 
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
+    data = request.json['data']
+    data_2d = np.array(list(data.values())).reshape(1,-1)
+    scaled_data_2d = scaler.transform(data_2d)
+    pred = model.predict(scaled_data_2d)[0]
+    output = np.round(pred,2)
+    return jsonify(output)
+
+
+
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.json['data']
-    print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
-    new_data = scaler.transform(np.array(list(data.values())).reshape(1,-1))
-    output = model.predict(new_data)
-    print(output)
-    return jsonify(output[0])
-
+    data = [float(x) for x in request.form.values()]
+    data_2d = np.array(data).reshape(1,-1)
+    scaled_data_2d = scaler.transform(data_2d)
+    pred = model.predict(scaled_data_2d)[0]
+    output = np.round(pred,2)
+    return render_template('home.html', prediction_text = "The House Price Prediction is {}".format(output))
 
 
 if __name__ == "__main__":
